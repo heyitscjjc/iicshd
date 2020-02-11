04935867727a0ac69ff25edbe060bb87c6e7fc27
rollandcharless <rollandcharless@users.noreply.github.com>
2020-02-11 10:53:02 +0800

    DONE ASSITANTHELPER DONE SYSTEM

--- iicshd/user/student/assisthelper.php
+++ iicshd/user/student/assisthelper.php
@@ -19,7 +19,7 @@ $style = "style='display:float;'";
 
     //CHATBOT COMMANDS
     function connectionError($errno, $errstr) {
-        echo "Something went wrong. Try again later.";
+        echo "Can't connect to the internet. Try again later.";
         die();
       }
     if(isset($_POST['send'])){
@@ -125,56 +125,50 @@ function sendMessage(){
     echo $reply;
 }
 
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../img/favicon.png">
-    <link rel="shortcut icon" href="img/favicon.png">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <title>IICS Help Desk</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="../../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../../css/dashboard.css" rel="stylesheet">
-    <link href="../../fa-5.5.0/css/fontawesome.css" rel="stylesheet">
-
-    <!-- Font Awesome JS -->
-    <script defer src="../../fa-5.5.0/js/solid.js"></script>
-    <script defer src="../../fa-5.5.0/js/fontawesome.js"></script>
-
-    <style>
-        .header {
-            padding: 10px;
-            text-align: center;
-            background: #2e2e2e;
-            color: white;
-            font-size: 30px;
-            position:fixed;
-            bottom:0;                
-            border-top: 5px solid #b00f24;
-        }
-    </style>
-
-    <link rel="stylesheet" href="../../DataTables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="../../DataTables/Responsive-2.2.1/css/responsive.bootstrap4.min.css">
-    <link rel="stylesheet" href="../../DataTables/Buttons-1.5.1/css/buttons.dataTables.min.css">
-</head>
-<body>
-<?php 
-    include '../../navbar.php';
 
 ?>
-<main>
 
-</main>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="../../img/favicon.png">
 
-<script type="text/javascript">
+        <title>IICS Help Desk</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="../../css/bootstrap.min.css" rel="stylesheet">
+        <link href="../../css/dashboard.css" rel="stylesheet">
+        <link href="../../fa-5.5.0/css/fontawesome.css" rel="stylesheet">
+
+        <style>
+            .header {
+                padding: 10px;
+                text-align: center;
+                background: #2e2e2e;
+                color: white;
+                font-size: 30px;
+                position:fixed;
+                bottom:0;                
+                border-top: 5px solid #b00f24;
+            }
+        </style>
+
+        <!-- Font Awesome JS -->
+        <script defer src="../../fa-5.5.0/js/solid.js"></script>
+        <script defer src="../../fa-5.5.0/js/fontawesome.js"></script>
+    </head>
+
+    <body>
+    
+    <?php 
+        include '../../navbar.php';
+    ?>
+
+	<script type="text/javascript">
     document.getElementById("query").focus();
 </script>
 
@@ -200,5 +194,61 @@ function sendMessage(){
         <br><button class="btn" name="send"<?php echo $style;?>>Send message</button>
     </form>
 </div>
-</body>
+
+        <div class="container-fluid header">
+            <div align="center" style="font-size: 11px; color:white;">
+                IICS Help Desk © 2019
+            </div>
+        </div>
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="../../js/jquery-3.3.1.js" ></script>
+        <script>window.jQuery || document.write('<script src="../../js/jquery-3.3.1.js"><\/script>')</script>
+        <script src="../../js/popper.js"></script>
+        <script src="../../js/bootstrap.min.js"></script>
+
+        <!-- Icons -->
+        <script src="../../js/feather.min.js"></script>
+        <script>
+            feather.replace()
+        </script>
+        <!-- Graphs -->
+		<script>
+            $(document).ready(function () {
+
+                function load_unseen_notification(view = '')
+                {
+                    $.ajax({
+                        url: "../../include/fetch2.php",
+                        method: "POST",
+                        data: {view: view},
+                        dataType: "json",
+                        success: function (data)
+                        {
+                            $('.shownotif').html(data.notification);
+                            if (data.unseen_notification > 0)
+                            {
+                                $('.count').html(data.unseen_notification);
+                            }
+                        }
+                    });
+                }
+
+                load_unseen_notification();
+
+                $(document).on('click', '.notif-toggle', function () {
+                    $('.count').html('');
+                    load_unseen_notification('yes');
+                });
+
+                setInterval(function () {
+                    load_unseen_notification();
+                    ;
+                }, 1000);
+
+            });
+        </script>
+    </body>
 </html>
